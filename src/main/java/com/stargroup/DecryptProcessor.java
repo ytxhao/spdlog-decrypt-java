1cbb69817fd3e9923b5132822cfc1dfc32ec8cfd
hao.yu <hao.yu@ushow.media>
2021-11-22 10:09:25 +0800

    格式化代码

--- src/main/java/com/stargroup/DecryptProcessor.java
+++ src/main/java/com/stargroup/DecryptProcessor.java
@@ -1,32 +1,32 @@
 package com.stargroup;
 
 import java.io.*;
-import java.net.URL;
 import java.util.Arrays;
 
 public class DecryptProcessor {
 
     // 数据头包含4字节的起始码，和四字节的加密数据长度
-    private static final byte[] DATA_HEADER = new byte[] {(byte)0x00, (byte)0x00, (byte)0x00, (byte)0x01, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00};
+    private static final byte[] DATA_HEADER = new byte[]{(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
 
     private static final byte[] KEY = new byte[]{
-            (byte)0xD6, (byte)0x71, (byte)0x3F, (byte)0xC0, (byte)0x65, (byte)0x87, (byte)0xD5, (byte)0x8B, (byte)0x56, (byte)0x3C, (byte)0x6E, (byte)0xF9, (byte)0xED, (byte)0xAD, (byte)0x24, (byte)0x92,
-            (byte)0x70, (byte)0xFC, (byte)0x96, (byte)0x10, (byte)0x89, (byte)0x9C, (byte)0xFA, (byte)0x45, (byte)0xB6, (byte)0x52, (byte)0xB,  (byte)0xCD, (byte)0x6C, (byte)0xF6, (byte)0x18, (byte)0x3A,
-            (byte)0xC0, (byte)0xBD, (byte)0xBF, (byte)0x5C, (byte)0x6C, (byte)0x3E, (byte)0xD3, (byte)0xCE, (byte)0xAD, (byte)0xEB, (byte)0x7A, (byte)0x2,  (byte)0x37, (byte)0x93, (byte)0x12, (byte)0x6F,
-            (byte)0x99, (byte)0xE0, (byte)0x9A, (byte)0xF9, (byte)0x28, (byte)0xC6, (byte)0xB2, (byte)0x8E, (byte)0x1B, (byte)0xBE, (byte)0x7E, (byte)0xCD, (byte)0x6C, (byte)0xEC, (byte)0xED, (byte)0xA5,
-            (byte)0xD6, (byte)0x71, (byte)0x3F, (byte)0xC0, (byte)0x65, (byte)0x87, (byte)0xD5, (byte)0x8B, (byte)0x56, (byte)0x3C, (byte)0x6E, (byte)0xF9, (byte)0xED, (byte)0xAD, (byte)0x24, (byte)0x92,
-            (byte)0x70, (byte)0xFC, (byte)0x96, (byte)0x10, (byte)0x89, (byte)0x9C, (byte)0xFA, (byte)0x45, (byte)0xB6, (byte)0x52, (byte)0xB,  (byte)0xCD, (byte)0x6C, (byte)0xF6, (byte)0x18, (byte)0x3A,
-            (byte)0xC0, (byte)0xBD, (byte)0xBF, (byte)0x5C, (byte)0x6C, (byte)0x3E, (byte)0xD3, (byte)0xCE, (byte)0xAD, (byte)0xEB, (byte)0x7A, (byte)0x2,  (byte)0x37, (byte)0x93, (byte)0x12, (byte)0x6F,
-            (byte)0x99, (byte)0xE0, (byte)0x9A, (byte)0xF9, (byte)0x28, (byte)0xC6, (byte)0xB2, (byte)0x8E, (byte)0x1B, (byte)0xBE, (byte)0x7E, (byte)0xCD, (byte)0x6C, (byte)0xEC, (byte)0xED, (byte)0xA5};
+            (byte) 0xD6, (byte) 0x71, (byte) 0x3F, (byte) 0xC0, (byte) 0x65, (byte) 0x87, (byte) 0xD5, (byte) 0x8B, (byte) 0x56, (byte) 0x3C, (byte) 0x6E, (byte) 0xF9, (byte) 0xED, (byte) 0xAD, (byte) 0x24, (byte) 0x92,
+            (byte) 0x70, (byte) 0xFC, (byte) 0x96, (byte) 0x10, (byte) 0x89, (byte) 0x9C, (byte) 0xFA, (byte) 0x45, (byte) 0xB6, (byte) 0x52, (byte) 0xB, (byte) 0xCD, (byte) 0x6C, (byte) 0xF6, (byte) 0x18, (byte) 0x3A,
+            (byte) 0xC0, (byte) 0xBD, (byte) 0xBF, (byte) 0x5C, (byte) 0x6C, (byte) 0x3E, (byte) 0xD3, (byte) 0xCE, (byte) 0xAD, (byte) 0xEB, (byte) 0x7A, (byte) 0x2, (byte) 0x37, (byte) 0x93, (byte) 0x12, (byte) 0x6F,
+            (byte) 0x99, (byte) 0xE0, (byte) 0x9A, (byte) 0xF9, (byte) 0x28, (byte) 0xC6, (byte) 0xB2, (byte) 0x8E, (byte) 0x1B, (byte) 0xBE, (byte) 0x7E, (byte) 0xCD, (byte) 0x6C, (byte) 0xEC, (byte) 0xED, (byte) 0xA5,
+            (byte) 0xD6, (byte) 0x71, (byte) 0x3F, (byte) 0xC0, (byte) 0x65, (byte) 0x87, (byte) 0xD5, (byte) 0x8B, (byte) 0x56, (byte) 0x3C, (byte) 0x6E, (byte) 0xF9, (byte) 0xED, (byte) 0xAD, (byte) 0x24, (byte) 0x92,
+            (byte) 0x70, (byte) 0xFC, (byte) 0x96, (byte) 0x10, (byte) 0x89, (byte) 0x9C, (byte) 0xFA, (byte) 0x45, (byte) 0xB6, (byte) 0x52, (byte) 0xB, (byte) 0xCD, (byte) 0x6C, (byte) 0xF6, (byte) 0x18, (byte) 0x3A,
+            (byte) 0xC0, (byte) 0xBD, (byte) 0xBF, (byte) 0x5C, (byte) 0x6C, (byte) 0x3E, (byte) 0xD3, (byte) 0xCE, (byte) 0xAD, (byte) 0xEB, (byte) 0x7A, (byte) 0x2, (byte) 0x37, (byte) 0x93, (byte) 0x12, (byte) 0x6F,
+            (byte) 0x99, (byte) 0xE0, (byte) 0x9A, (byte) 0xF9, (byte) 0x28, (byte) 0xC6, (byte) 0xB2, (byte) 0x8E, (byte) 0x1B, (byte) 0xBE, (byte) 0x7E, (byte) 0xCD, (byte) 0x6C, (byte) 0xEC, (byte) 0xED, (byte) 0xA5};
 
     private static final int MAX_READ_LEN = 8192;
+
     public static void main(String[] args) {
         String inputFilePath;
         String outputFilePath;
         if (args.length < 1) {
             System.out.println("missing parameter!");
             return;
-        } else if (args.length == 1){
+        } else if (args.length == 1) {
             inputFilePath = args[0];
             int pos = inputFilePath.lastIndexOf(".");
             if (pos < 0) {
@@ -60,10 +60,7 @@ public class DecryptProcessor {
         try {
             randomFile = new RandomAccessFile(inputFile, "r");
             randomFile.seek(0);
-            // 文件长度，字节数
             long fileLength = randomFile.length();
-//            long fileCurrentIndex = randomFile.getFilePointer();
-//            System.out.println("fileLength:" + fileLength + " fileCurrentIndex:" + fileCurrentIndex);
             out = new FileOutputStream(outputFile);
 
             if (!outputFile.exists()) {
@@ -79,14 +76,13 @@ public class DecryptProcessor {
                     if (headerBuf[0] == DATA_HEADER[0] && headerBuf[1] == DATA_HEADER[1] && headerBuf[2] == DATA_HEADER[2] && headerBuf[3] == DATA_HEADER[3]) {
                         index = 0;
                         int readLen = randomFile.read(readBuf, 0, 4);
-                        if (readLen < 4){
+                        if (readLen < 4) {
                             System.out.println("no more data! readLen:" + readLen);
                             break;
                         }
                         long readFilePos = randomFile.getFilePointer();
-                        System.arraycopy(readBuf, 0, realEncryptLenByte,0, realEncryptLenByte.length);
+                        System.arraycopy(readBuf, 0, realEncryptLenByte, 0, realEncryptLenByte.length);
                         int realEncryptLen = bytesToInt(realEncryptLenByte);
-//                        System.out.println("realEncryptLen:" + realEncryptLen + " readFilePos:" + readFilePos + " fileLength - readFilePos="+(fileLength - readFilePos));
                         if (realEncryptLen > fileLength - readFilePos) {
                             String err = "error: xor decrypt header data error!!! skip this line in log file\n";
                             out.write(err.getBytes(), 0, err.length());
@@ -101,7 +97,7 @@ public class DecryptProcessor {
 
                         Arrays.fill(readBuf, (byte) 0);
                         readLen = randomFile.read(readBuf, 0, realEncryptLen);
-                        if (readLen < realEncryptLen){
+                        if (readLen < realEncryptLen) {
                             System.out.println("no more data!! readLen:" + readLen);
                             break;
                         }
@@ -110,19 +106,17 @@ public class DecryptProcessor {
                         Arrays.fill(readBuf, (byte) 0);
                         randomFile.seek(readFilePos);
                     } else {
-                        byte[] tmp = Arrays.copyOfRange(headerBuf, 1,4);
+                        byte[] tmp = Arrays.copyOfRange(headerBuf, 1, 4);
                         System.arraycopy(tmp, 0, headerBuf, 0, 3);
                     }
                 } else {
                     index++;
                 }
-//                    System.out.println("readByte:"+Integer.toHexString(readBuf[0]));
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-//                    System.out.println("out.close "+(out != null));
                 if (out != null) {
                     out.close();
                     out = null;
@@ -131,7 +125,6 @@ public class DecryptProcessor {
                 e.printStackTrace();
             }
             try {
-//                    System.out.println("in.close "+(in != null));
                 if (randomFile != null) {
                     randomFile.close();
                     randomFile = null;
